dfdaa8b1ee6fe028bc9456646bf4ec64df022a24
cwang81 <chenghaowangbc400@gmail.com>
2021-07-25 17:16:35 -0500

    update MySQLConnection

---
+++ src/main/java/com/laioffer/giligili/servlet/FavoriteServlet.java
@@ -0,0 +1,87 @@
+package com.laioffer.giligili.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.giligili.db.MySQLConnection;
+import com.laioffer.giligili.db.MySQLException;
+import com.laioffer.giligili.entity.FavoriteRequestBody;
+import com.laioffer.giligili.entity.Item;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "FavoriteServlet", value = "/favorite")
+public class FavoriteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userId = request.getParameter("user_id");
+        Map<String, List<Item>> itemMap;
+        MySQLConnection connection = null;
+        try {
+            // Read the favorite items from the database
+            connection = new MySQLConnection();
+            itemMap = connection.getFavoriteItems(userId);
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().print(new ObjectMapper().writeValueAsString(itemMap));
+        } catch (MySQLException e) {
+            throw new ServletException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // get user ID from request URL, this is a temporary solution since we don’t support session now
+        String userId = request.getParameter("user_id");
+        // get favorite item information from request body
+        ObjectMapper mapper = new ObjectMapper();
+        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        if (body == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        MySQLConnection connection = null;
+        try {
+            // Save the favorite item to the database
+            connection = new MySQLConnection();
+            connection.setFavoriteItem(userId, body.getFavoriteItem());
+        } catch (MySQLException e) {
+            throw new ServletException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userId = request.getParameter("user_id");
+        ObjectMapper mapper = new ObjectMapper();
+        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        if (body == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        MySQLConnection connection = null;
+        try {
+            // Remove the favorite item to the database
+            connection = new MySQLConnection();
+            connection.unsetFavoriteItem(userId, body.getFavoriteItem().getId());
+        } catch (MySQLException e) {
+            throw new ServletException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
