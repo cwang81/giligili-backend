ff08929a51f4ffbf02ab8736d367f8e1d5f2ad2c
cwang81 <chenghaowangbc400@gmail.com>
2021-07-28 23:11:49 -0500

    set up authentication

--- src/main/java/com/laioffer/giligili/db/MySQLConnection.java
+++ src/main/java/com/laioffer/giligili/db/MySQLConnection.java
@@ -2,6 +2,7 @@ package com.laioffer.giligili.db;
 
 import com.laioffer.giligili.entity.Item;
 import com.laioffer.giligili.entity.ItemType;
+import com.laioffer.giligili.entity.User;
 
 import java.sql.*;
 import java.util.*;
@@ -176,4 +177,44 @@ public class MySQLConnection {
         return itemMap;
     }
 
+    // Verify if the given user Id and password are correct. Returns the user name when it passes
+    public String verifyLogin(String userId, String password) throws MySQLException {
+        String name = "";
+        String sql = "SELECT first_name, last_name FROM users WHERE id = ? AND password = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            statement.setString(2, password);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                name = rs.getString("first_name") + " " + rs.getString("password");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to verify login.");
+        }
+        return name;
+    }
+
+    // Add a new user to the database
+    public boolean addUser(User user) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+
+        String sql = "INSERT IGNORE INTO users VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, user.getUserId());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getFirstName());
+            statement.setString(4, user.getLastName());
+
+            return statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to add user to database.");
+        }
+    }
 }
